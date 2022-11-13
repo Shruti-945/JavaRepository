6cfa558256fa9b2f6d39404871d2ee8b5b3c845d
Shruti-945 <110717166+Shruti-945@users.noreply.github.com>
2022-11-13 11:35:37 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+public class BinarySearch {
+    public static void main(String[] args) {
+        //Binary search is used only for sorted array.
+        int[] arr={2,4,6,8,9,12};
+        int target=12;
+        int ans=Binarysearch(arr, target);
+        System.out.println("The index of the target element is " +ans);
+}
+    //Return the index.
+    //Return -1 if it does not exist.
+    static int Binarysearch(int[] arr,int target) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end) {
+        //Find the middle element.
+       // int mid=(start+end)/2;   //It might be possible that start+end exceeds the range of integer value.
+       int mid=start+(end-start)/2;  //Same thing as above. Just check this out.
+       if(target<arr[mid]) {
+        end=mid-1;
+       }
+       else if(target>arr[mid]) {
+        start=mid +1;
+       }
+       else if(target==arr[mid]) {
+        //Answer found.
+        return mid;
+       }
+    }
+    return-1;
+ }
+}
